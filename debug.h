ed6ab551b51e9a16e1c5cf3a781b988c144cbb64
Clayton Kuchta <clayton.kuchta@gmail.com>
2014-10-09 13:15:47 -0400

    Add Debug Header File

---
+++ debug.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   debug.h
+ * Author: s45504
+ *
+ * Created on October 9, 2014, 1:15 PM
+ */
+
+#ifndef DEBUG_H
+#define	DEBUG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DEBUG_H */
+
